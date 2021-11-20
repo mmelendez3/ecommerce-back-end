4f0ff10938bef31531a77c854e6180fd50e59d75
mmelendez3 <mele0019@gmail.com>
2021-11-19 20:21:17 -0600

    added get all routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,8 +4,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Product.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id', 'product_id', 'tag_id']
+      },
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
